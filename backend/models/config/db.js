3cfe51f97b5c82910e41ed46eda58ba22e647f8a
Ankit Kumar <153090276+Ankit-euphemism@users.noreply.github.com>
2025-08-22 13:50:52 +0530

    Update db.js

--- backend/models/config/db.js
+++ backend/models/config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql from "mysql2"
 import dotenv from "dotenv"
 dotenv.config();
@@ -15,22 +14,4 @@ db.connect((err)=>{
     console.log("MySQL connect...");
 });
 
-=======
-import mysql from "mysql2"
-import dotenv from "dotenv"
-dotenv.config();
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST||"localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-}).promise();
-
-db.connect((err)=>{
-    if(err) console.log(err);
-    console.log("MySQL connect...");
-});
-
->>>>>>> f1ea9143b0b6eda64ff87e8b7b2704192c6b5400
 export default db;
