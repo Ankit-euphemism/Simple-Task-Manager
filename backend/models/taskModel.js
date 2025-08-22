ce6cc115649371979e8e33d77e37345255ade700
Ankit Kumar <153090276+Ankit-euphemism@users.noreply.github.com>
2025-08-22 13:50:24 +0530

    Update taskModel.js

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import db from "./config/db.js";
 
 export async function getTasks() {
@@ -54,61 +53,5 @@ export async function deleteTask(id) {
 }
 
 // const result= await getTask(1);
-=======
-import db from "./config/db.js";
-
-export async function getTasks() {
-  const [rows] = await db.query("SELECT * FROM tasks");
-  return rows;
-}
-
-export async function getTask(id) {
-  const [row] = await db.query(
-    `
-    SELECT * FROM tasks
-    WHERE id = ?`,
-    [id]
-  );
-  return row;
-}
-
-export async function createTask(title, completed) {
-  const [create] = await db.query(
-    `
-    INSERT INTO tasks(title,completed)
-    VALUES(?,?)`,
-    [title, completed]
-  );
-  return getTask(create.insertId);
-}
-
-export async function updateTask(id, title, completed) {
-  const [update] = await db.query(
-    `
-    UPDATE tasks
-    SET title=?,completed=?
-    WHERE id=?`,
-    [title, completed, id]
-  );
-  return getTask(id);
-}
-
-export async function deleteTask(id) {
-  const [deleteTask] = await db.query(
-    `
-    DELETE FROM tasks
-    WHERE id = ?`,
-    [id]
-  );
-
-  const [updateId] = await db.query(
-    `
-    ALTER TABLE tasks AUTO_INCREMENT = 1;
-    `
-  );
-  return getTask(id);
-}
-
 // const result= await getTask(1);
->>>>>>> f1ea9143b0b6eda64ff87e8b7b2704192c6b5400
 // console.log(result);
