4f3e696bdaeb0d5643c7db1fc3ac060d162690f6
Ankit-euphemism <ankitkumarb47@bbdu.ac.in>
2025-09-13 14:04:16 +0530

    add changes

--- create-db-template.sql
+++ create-db-template.sql
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 use taskmanager;
 
 create table tasks(
@@ -11,18 +9,4 @@ create table tasks(
 
 select * from tasks;
 
-=======
-
-use taskmanager;
-
-create table tasks(
-    id int primary key auto_increment,
-    title varchar(255),
-    completed bool default false,
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-select * from tasks;
-
->>>>>>> f1ea9143b0b6eda64ff87e8b7b2704192c6b5400
 describe tasks;
